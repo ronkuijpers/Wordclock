486fdbbaa6f74d540b1970d941648907946cf1ac
Ron Kuijpers <ronkuijpers@gmail.com>
2025-10-31 23:01:11 +0100

    fix in firmware.json

--- include/build_info.h
+++ include/build_info.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define BUILD_GIT_SHA "81e3a0e"
-#define BUILD_TIME_UTC "2025-10-31T21:11:14Z"
+#define BUILD_GIT_SHA "c3f0abe"
+#define BUILD_TIME_UTC "2025-10-31T21:54:19Z"
 #define BUILD_ENV_NAME "esp32dev"
