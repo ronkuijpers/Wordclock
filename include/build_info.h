fa3992997aef97397aac2fd610ae4518991ed92a
Ron Kuijpers <ronkuijpers@gmail.com>
2025-11-21 21:48:26 +0100

    v2025.11.21 build info

--- include/build_info.h
+++ include/build_info.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define BUILD_GIT_SHA "c426605"
-#define BUILD_TIME_UTC "2025-11-21T20:42:36Z"
+#define BUILD_GIT_SHA "e0f87be"
+#define BUILD_TIME_UTC "2025-11-21T20:48:15Z"
 #define BUILD_ENV_NAME "esp32dev"
