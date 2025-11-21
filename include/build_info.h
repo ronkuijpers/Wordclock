c3554f1cebc314eebde9cc772975a045833c4034
Ron Kuijpers <ronkuijpers@gmail.com>
2025-11-21 21:44:02 +0100

    back to 0.13 for a bit

--- include/build_info.h
+++ include/build_info.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define BUILD_GIT_SHA "c3f0abe"
-#define BUILD_TIME_UTC "2025-10-31T21:54:19Z"
+#define BUILD_GIT_SHA "c426605"
+#define BUILD_TIME_UTC "2025-11-21T20:42:36Z"
 #define BUILD_ENV_NAME "esp32dev"
