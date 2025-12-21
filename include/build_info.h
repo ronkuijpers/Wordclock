d0b3756a8402a41787c0363e345000c60a6dd48e
Ron Kuijpers <ronkuijpers@gmail.com>
2025-12-21 19:36:31 +0100

    some build changes - nothing relevant

--- include/build_info.h
+++ include/build_info.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define BUILD_GIT_SHA "530a9e6"
-#define BUILD_TIME_UTC "2025-12-21T18:22:40Z"
+#define BUILD_GIT_SHA "df469a7"
+#define BUILD_TIME_UTC "2025-12-21T18:28:33Z"
 #define BUILD_ENV_NAME "esp32dev"
