83ac3c0238016a08965b85b2f1e17a350e33b3d2
Ron Kuijpers <ronkuijpers@gmail.com>
2025-12-28 10:47:17 +0100

    2025.12.28_develop

--- src/config.h
+++ src/config.h
@@ -3,8 +3,8 @@
 #define LOG_BUFFER_SIZE 150
 #pragma once
 
-#define FIRMWARE_VERSION "2025.12.27_develop"
-#define UI_VERSION "2025.12.27_develop"
+#define FIRMWARE_VERSION "2025.12.28_develop"
+#define UI_VERSION "2025.12.28_develop"
 
 #define DATA_PIN 4
 #define DEFAULT_BRIGHTNESS 5
