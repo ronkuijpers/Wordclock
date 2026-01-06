adb561567a776ceb0c2116c57020ba6220aa2507
Ron Kuijpers <ronkuijpers@gmail.com>
2026-01-06 13:24:07 +0100

    Bump version to v26.2.0-dev.18

--- src/config.h
+++ src/config.h
@@ -3,8 +3,8 @@
 #define LOG_BUFFER_SIZE 150
 #pragma once
 
-#define FIRMWARE_VERSION "26.2.0-dev.17"
-#define UI_VERSION "26.2.0-dev.17"
+#define FIRMWARE_VERSION "26.2.0-dev.18"
+#define UI_VERSION "26.2.0-dev.18"
 
 #define DATA_PIN 4
 #define DEFAULT_BRIGHTNESS 5
