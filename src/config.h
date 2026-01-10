8f3648c46bb61c3893e38b1f8a111f9a514bdfb7
Ron Kuijpers <ronkuijpers@gmail.com>
2026-01-10 16:29:12 +0100

    Bump version to v26.2.2-dev.13

--- src/config.h
+++ src/config.h
@@ -3,8 +3,8 @@
 #define LOG_BUFFER_SIZE 50  // Reduced from 150 to save flash space
 #pragma once
 
-#define FIRMWARE_VERSION "26.2.2-dev.12"
-#define UI_VERSION "26.2.2-dev.12"
+#define FIRMWARE_VERSION "26.2.2-dev.13"
+#define UI_VERSION "26.2.2-dev.13"
 
 #define DATA_PIN 4
 #define DEFAULT_BRIGHTNESS 5
